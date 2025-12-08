810c4bb265868f46a625c1f5364ddb5ac9cb2dd8
Vishwa <vishwadipsingale99@gmail.com>
2025-12-08 10:34:49 +0000

    change in data_loader

--- src/data_loader.py
+++ src/data_loader.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 def load_data(path="data/cleaned_ci_cd_logs.csv"):
     df = pd.read_csv(path)
 
-    # Choose features – only numeric + enc columns, no raw text
+    
     feature_cols = [
         "pipeline_id_enc", "stage_name_enc", "job_name_enc",
         "task_name_enc", "branch_enc", "user_enc", "environment_enc",
@@ -12,7 +12,7 @@ def load_data(path="data/cleaned_ci_cd_logs.csv"):
         "is_user_unknown", "is_env_unknown", "job_fail_count"
     ]
     X = df[feature_cols]
-    y = df["target"]   # 0 = non-fail, 1 = fail
+    y = df["target"]  
 
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42, stratify=y
